fe5ada24758f3c4cba7166974c7869c16e41e101
Sam Shi <samshi@Sams-MacBook-Pro.local>
2020-12-07 21:50:14 -0500

    added display for rating and type of food

--- index.py
+++ index.py
@@ -1,16 +1,37 @@
-from selenium import webdriver
 import time
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.yelp.com/search'
-location='?find_loc=' + input("Enter a location: ") # get input from javascript
-page = requests.get(URL)
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
 
-soup = BeautifulSoup(page.content, 'html.parser')
+# location=input("Enter a location: ") # add eventually: get input from javascript
+item_start_number = 0
+for i in range(10):
+    URL = 'https://www.yelp.com/search?cflt=restaurants&find_loc=Indianapolis&start=' + str(item_start_number*10)
+    page = requests.get(URL,headers=headers)
+    soup = BeautifulSoup(page.content, 'lxml')
+    for item in soup.select('[class*=container]'):
+        try:
+            if(item.find('h4')):
+                # filters out sponsored restaurants
+                name = item.find('h4').get_text()            
+                if(name[0].isnumeric()):
+                    rating = soup.select('[aria-label*=rating]')[0]['aria-label']
+                    food_type = item.select('div div div div div div div div div span span span a')[0].get_text()
+                    print(name)
+                    print(rating)
+                    print(food_type)
+                    print('*********************')
+                
+        except Exception as e:
+            raise e
+            print('')
+    item_start_number+=1
+
 # print(soup.prettify())
-results = soup.find_all('span', class_=' raw__09f24__3Obuy')
-print(results)
+# results = soup.find_all('span', class_=' raw__09f24__3Obuy')
+
+# print(results.prettify())
 # biztitle = results.find_all('a',class_=' link__09f24__1kwXV link-color--inherit__09f24__3PYlA link-size--inherit__09f24__2Uj95')
 # print(results)
 # import requests
